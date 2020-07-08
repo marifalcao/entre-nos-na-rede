84cd0481e5f5d5128a974484ef07ac999d858e4e
Mariana Falcão <marianadealbfalcao@gmail.com>
2020-07-07 21:52:27 -0300

    iniciando pagina de cadastro

---
+++ pages/cadastro.php
@@ -0,0 +1,89 @@
+<div class="container">
+    <div class="w-100 text-center mt-4">
+        <h1 class="font-bold mb-5">Cadastro</h1>
+    </div>
+
+    <section class="text-center">
+        <div class="section-title">Dados pessoais</div>
+        <div class="card">
+            <div class="form-row">
+                <div class="col-12">
+                    <div class="form-group">
+                        <label for="nome" class="d-none"></label>
+                        <input class="form-control" id="nome" type="password" placeholder="Nome Completo">
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="form-group mb-md-0 text-md-left"">
+                        <label for="cpf" class="d-none"></label>
+                        <input class="form-control" id="cpf" type="password" placeholder="CPF">
+                        <small class="text-muted">* Essa informação não é visível para os outros usuários</small>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="form-group mb-md-0 text-md-left">
+                        <label for="nascimento" class="d-none"></label>
+                        <input class="form-control" id="nascimento" type="password" placeholder="Data de Nascimento">
+                        <small class="text-muted">* Essa informação não é visível para os outros usuários</small>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <section class="text-center mt-3">
+        <div class="section-title">Dados profissionais</div>
+        <div class="card">
+            <div class="form-row">
+                <div class="col-12">
+                    <div class="form-group">
+                        <label for="nome" class="d-none"></label>
+                        <input class="form-control" id="nome" type="password" placeholder="Nome Completo">
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="form-group mb-md-0 text-md-left"">
+                    <label for="cpf" class="d-none"></label>
+                    <input class="form-control" id="cpf" type="password" placeholder="CPF">
+                    <small class="text-muted">* Essa informação não é visível para os outros usuários</small>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="form-group mb-md-0 text-md-left">
+                    <label for="nascimento" class="d-none"></label>
+                    <input class="form-control" id="nascimento" type="password" placeholder="Data de Nascimento">
+                    <small class="text-muted">* Essa informação não é visível para os outros usuários</small>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="text-center mt-3">
+        <div class="section-title">Dados da Conta</div>
+        <div class="card">
+            <div class="form-row">
+                <div class="col-12">
+                    <div class="form-group">
+                        <label for="nome" class="d-none"></label>
+                        <input class="form-control" id="nome" type="password" placeholder="Nome Completo">
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="form-group mb-md-0 text-md-left"">
+                    <label for="cpf" class="d-none"></label>
+                    <input class="form-control" id="cpf" type="password" placeholder="CPF">
+                    <small class="text-muted">* Essa informação não é visível para os outros usuários</small>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="form-group mb-md-0 text-md-left">
+                    <label for="nascimento" class="d-none"></label>
+                    <input class="form-control" id="nascimento" type="password" placeholder="Data de Nascimento">
+                    <small class="text-muted">* Essa informação não é visível para os outros usuários</small>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="w-100 d-flex justify-content-end">
+        <button type="button" class="btn btn-primary btn-lg my-4">Cadastrar</button>
+    </div>
+</div>
